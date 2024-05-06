8d66baca1a4c7cc81e63873c144098fc41baa386
Antonio Gil <agilgdam@gmail.com>
2024-05-06 11:10:51 +0200

    feat(practica03): :construction: Antonio Gil · añade el modelo de datos de la capa de negocio

---
+++ AntonioGil/practica03/src/main/java/com/viewnext/bsan/practica03/bo/UserBo.java
@@ -0,0 +1,28 @@
+package com.viewnext.bsan.practica03.bo;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+/**
+ * The {@code UserBo} business object class represents the users registered in the system.
+ *
+ * @author Antonio Gil
+ */
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class UserBo {
+
+    @EqualsAndHashCode.Include
+    String dni;
+
+    String name;
+
+    String surname;
+
+    int age;
+
+}
