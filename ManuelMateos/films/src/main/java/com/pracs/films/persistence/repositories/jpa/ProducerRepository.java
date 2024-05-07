b9e6218fbfc8d385d6f0a762dc2ede260549057b
Manumdt <mmateosdt@gmail.com>
2024-05-07 09:29:48 +0200

    docs: :memo: Documentation

--- ManuelMateos/films/src/main/java/com/pracs/films/persistence/repositories/jpa/ProducerRepository.java
+++ ManuelMateos/films/src/main/java/com/pracs/films/persistence/repositories/jpa/ProducerRepository.java
@@ -3,5 +3,11 @@ package com.pracs.films.persistence.repositories.jpa;
 import com.pracs.films.persistence.models.Producer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository of the class {@link Producer}
+ *
+ * @author Manuel Mateos de Torres
+ * @see {@link JpaRepository}
+ */
 public interface ProducerRepository extends JpaRepository<Producer, Long> {
 }
