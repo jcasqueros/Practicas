85a6a3a31be4c538d5a9fcdfbfd550c8ea1c2af6
jcasqueros <jorge.casquero@viewnext.com>
2024-05-09 10:10:34 +0200

    feat: :rocket: miercoles 9/5

---
+++ JorgeCasquero/Practica4/src/test/java/com/example/demo/service/ProductoraServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.demo.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductoraServiceImpl {
+
+	@Test
+	void testGetAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAllCriteria() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetByIdCriteria() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateCriteria() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteByIdCriteria() {
+		fail("Not yet implemented");
+	}
+
+}
