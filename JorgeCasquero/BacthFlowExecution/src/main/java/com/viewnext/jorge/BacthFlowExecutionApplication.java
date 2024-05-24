6348f14440b0bccebc0f3c332e981c22ff8c96ff
vjp-jorgecs <72507397+vjp-jorgecs@users.noreply.github.com>
2024-05-24 14:53:03 +0200

    feat: :art:

---
+++ JorgeCasquero/BacthFlowExecution/src/main/java/com/viewnext/jorge/BacthFlowExecutionApplication.java
@@ -0,0 +1,13 @@
+package com.viewnext.jorge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacthFlowExecutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacthFlowExecutionApplication.class, args);
+	}
+
+}
