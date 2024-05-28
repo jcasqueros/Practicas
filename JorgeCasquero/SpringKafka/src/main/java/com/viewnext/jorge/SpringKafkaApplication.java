e729b3960ba8c9dee39c60616b1f7a35f69b1994
vjp-jorgecs <72507397+vjp-jorgecs@users.noreply.github.com>
2024-05-28 17:51:35 +0200

    inicioKafka

---
+++ JorgeCasquero/SpringKafka/src/main/java/com/viewnext/jorge/SpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.viewnext.jorge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaApplication.class, args);
+	}
+
+}
