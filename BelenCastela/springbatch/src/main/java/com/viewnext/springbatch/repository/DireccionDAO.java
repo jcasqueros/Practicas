10d7ffe6e356edd76493712c7dc97292df3be81e
NullJellyfish <130540326+NullJellyfish@users.noreply.github.com>
2024-05-29 20:19:17 +0200

    -

--- BelenCastela/springbatch/src/main/java/com/viewnext/springbatch/repository/DireccionDAO.java
+++
@@ -1,9 +0,0 @@
-package com.viewnext.springbatch.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.viewnext.springbatch.model.Direccion;
-
-public interface DireccionDAO extends CrudRepository<Direccion, Long>{
-
-}
--- BelenCastela/springbatch/src/main/java/com/viewnext/springbatch/repository/DistritoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.viewnext.springbatch.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.viewnext.springbatch.model.Distrito;
-
-public interface DistritoDAO extends CrudRepository<Distrito, Long>{
-
-}
