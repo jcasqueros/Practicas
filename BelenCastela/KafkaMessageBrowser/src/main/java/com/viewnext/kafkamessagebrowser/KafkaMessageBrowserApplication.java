95a0c2bc8d796c46d73812f262457b912216e556
NullJellyfish <130540326+NullJellyfish@users.noreply.github.com>
2024-06-13 13:17:11 +0200

    Unnecessary tag out-

--- BelenCastela/KafkaMessageBrowser/src/main/java/com/viewnext/kafkamessagebrowser/KafkaMessageBrowserApplication.java
+++ BelenCastela/KafkaMessageBrowser/src/main/java/com/viewnext/kafkamessagebrowser/KafkaMessageBrowserApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@ComponentScan
 public class KafkaMessageBrowserApplication {
 	
 	@Bean
